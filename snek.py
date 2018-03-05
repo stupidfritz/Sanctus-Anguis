15563ac774e9455e98ba08d1f08158143bd5d70d
stupidfritz <aydinsesker@outlook.com>
2018-03-04 19:05:12 -0800

    Add files via upload

--- snek.py
+++ snek.py
@@ -4,7 +4,6 @@ from pygame.locals import *
 
 class Snek:
     def __init__(self, window, width, height, color, x, y):
-        self.window = window
         self.width = width
         self.height = height
         self.color = color
